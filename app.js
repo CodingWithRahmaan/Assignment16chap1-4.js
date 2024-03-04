30edb7d5ab76c51dbc87b7588e9839072bd9f577
CodingWithRahmaan <abdurrehmanbasra1@gmail.com>
2024-03-04 14:54:29 -0800

    .

---
+++ app.js
@@ -0,0 +1,98 @@
+//Chapter 01
+// Queston No. 01
+// Queston No. 02
+var valid_Password = "Error! Please enter a valid password.";
+alert(valid_Password);
+// Queston No. 03
+var welCome = "Welcome to JavaScript Land.";
+var coding = "Happy Coding!";
+alert(welCome + " \n " + coding);
+// Queston No. 04
+alert(welCome);
+var dialog = "Prevent this page from additional dialogs.";
+alert(coding + " \n " + dialog );
+// Queston No. 05
+console.log("Hello...I can run JS through my web browser's console")
+
+
+
+
+
+
+//Chapter 02
+
+// Question No. 01
+var userName = "A Rahmaan";
+// Question No. 02
+var myName = userName;
+// Question No. 03
+var message;
+message = "Hello World"
+alert(message);
+// Question No. 04
+var student_Name = "Abdur rahmaan";
+var student_Age = "18";
+var certification = "Web and app developer"
+alert(student_Name);
+alert(student_Age + " years old");
+alert(certification);
+// Question No. 05
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+// Question No. 06
+var email = "abdurrehmanbasra1@gmail.com"
+alert("My email address is " + email);
+// Question No. 07
+var book = "A smart way to learn JavaScript.";
+alert("I am trying to learn from the book " + book);
+// Question No. 08
+document.write("Yah! I can write HTML content through JavaScript");
+// Question No. 09
+var dummy_string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(dummy_string)
+document.write(dummy_string)
+
+
+//Chapter 03
+//Question No. 01
+// number variable already declare
+alert("I am " + student_Age + " year old");
+//Question No. 02
+var visit= 69;
+alert("You have visisted this site " + visit + " times");
+//Question No. 03
+var dob = 2005;
+document.write("<br> My birth year is " + dob);
+document.write("<br> Date type of my declare variable is number")
+//Question No. 04
+var visitor_Name = "Abdur Rahmaan";
+var product_Title = "Ball";
+var purchase_quantity = 68 + 1;
+document.write("<br>" + visitor_Name + " ordered " + purchase_quantity + product_Title + "(s) on Pochinki warehouse");
+
+//Chapter 04
+//Question No. 01
+var first_Name = "Abdur", last_Name = "Rahmaan", full_Name = first_Name + " " + last_Name;
+//Question No. 02
+
+
+var customer_Number = 8348;
+var customer_Name = "hassan";
+var customer_Dob = 2006;
+var customer_City = "Karachi";
+var customer_Status = "Active";
+
+
+//Question No. 03
+
+var rules = "Rules for naming JS variables";
+document.write("<h1> " + rules + " </h1>");
+var numbers = "numbers";
+var $sign = "$";
+var _sign = "_";
+document.write("<br> Variable names can only contain" + numbers+ "," + $sign + "," + " and " + _sign + "." + " For example : " + $sign + "my" + _sign + "$my_1stVariable" );
+document.write("<br> Variables must begin with a " + $sign+ " or " + _sign + " For example : " + $sign + "name" + "," + _sign + "name or name" );
+var variable_Name = "sensitive";
+document.write("<br> Variable names are case " + variable_Name);
+var js_Keywords = "keywords";
+document.write("<br> Variable names should not be JS " + js_Keywords);
